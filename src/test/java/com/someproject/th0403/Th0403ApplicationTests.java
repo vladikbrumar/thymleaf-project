d61f6efe200b41c3b7fe5b8e52f2868304006f6a
vladikbrumar <vladikbraun@gmail.com>
2019-03-04 14:59:25 +0200

    update files

---
+++ src/test/java/com/someproject/th0403/Th0403ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.someproject.th0403;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Th0403ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
